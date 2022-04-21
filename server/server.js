93b61c99897a00508591fca6be48ea8d2442a9a6
Twigster <treshagaye@gmail.com>
2022-04-20 21:58:20 -0400

    set up thought & user resolvers & typedefs

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
-// const { authMiddleware } = require('./utils/auth');
 
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
