f45d6069600c48f16ebf2279da5eac1498b5f9e2
Twigster <treshagaye@gmail.com>
2022-04-23 01:04:06 -0400

    set up concurrently for client & server

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="w-100 mt-auto bg-secondary p-4">
+      <div className="container">&copy;2022 by Tresha-Gaye Ustanny</div>
+    </footer>
+  );
+};
+
+export default Footer;
